42e7dd3cdad5b705d8b34cac099d9ad94864f81c
Marius Metzger <marius@crushedpixel.net>
2026-09-09 07:49:19 +0200

    Support clap-wrapper next branch in AUv2 entrypoints

--- src/auv2-cpp/generated_entrypoints.hxx
+++ src/auv2-cpp/generated_entrypoints.hxx
@@ -1,29 +1,15 @@
 #pragma once
 #include "detail/auv2/auv2_base_classes.h"
 
-AUV2_Type typeFromAUInstance(AudioComponentInstance *ci)
-{
-    AudioComponent comp = AudioComponentInstanceGetComponent(*ci);
-    AudioComponentDescription desc;
-
-    if (comp == nullptr || AudioComponentGetDescription(comp, &desc) != 0)
-        return AUV2_Type::aumu_musicdevice;
-
-    if (desc.componentType == 'aufx')
-        return AUV2_Type::aufx_effect;
-    if (desc.componentType == 'aumi' || desc.componentType == 'aumf')
-        return AUV2_Type::aumi_noteeffect;
-
-    return AUV2_Type::aumu_musicdevice;
-}
-
-#define CLAP_AUV2_ENTRY(n)                                           \
-    struct wrapAsAUV2_inst##n : free_audio::auv2_wrapper::WrapAsAUV2 \
-    {                                                                \
-        wrapAsAUV2_inst##n(AudioComponentInstance ci)                \
-            : free_audio::auv2_wrapper::WrapAsAUV2(                  \
-                  typeFromAUInstance(&ci), "", "", n, ci) {}         \
-    };                                                               \
+/// Defines an AU factory entry for one CLAP plugin index.
+#define CLAP_AUV2_ENTRY(n)                                                        \
+    /** Wraps one CLAP plugin using the host's component identity. */             \
+    struct wrapAsAUV2_inst##n : free_audio::auv2_wrapper::WrapAsAUV2               \
+    {                                                                            \
+        /** Initializes the wrapper for this plugin index and host instance. */  \
+        wrapAsAUV2_inst##n(AudioComponentInstance ci)                              \
+            : free_audio::auv2_wrapper::WrapAsAUV2("", "", n, ci) {}               \
+    };                                                                           \
     AUSDK_COMPONENT_ENTRY(ausdk::AUMusicDeviceFactory, wrapAsAUV2_inst##n);
 
 CLAP_AUV2_ENTRY(0);
